4ba8df829315f73ab2b19a296a53d405774420a2
stringyang <stringyang@126.com>
2021-05-17 23:55:10 +0800

    feat: 使用新思路实现输入框扩展。

---
+++ src/components/xi-input-ext/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <xi-input v-bind="$attrs"></xi-input>
+    <xi-input-ext-frame></xi-input-ext-frame>
+</template>
+
+<script setup lang="ts">
+import { XiInput } from "../11th"
+import XiInputExtFrame from "./XiInputExtFrame.vue"
+
+</script>
+
+<style scoped>
+</style>
