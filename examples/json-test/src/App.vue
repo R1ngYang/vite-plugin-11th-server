68916f585620d1557b0ce68a8816d26ae95321d6
stringyang <stringyang@126.com>
2022-06-11 23:51:07 +0800

    feat: 增加示例。

--- examples/json-test/src/App.vue
+++ examples/json-test/src/App.vue
@@ -1,34 +1,7 @@
 <template>
-  <button @click="handleGet">发送get请求</button>
-  <div>{{ getContent }}</div>
-  <br />
-  <button @click="handlePost">发送post请求</button>
-  <div>{{ postContent }}</div>
+  <div>123</div>
 </template>
 
-<script setup lang="ts">
-  import { ref } from 'vue';
-
-  const getContent = ref();
-  function handleGet() {
-    fetch('hello')
-      .then((response) => response.text())
-      .then((data) => {
-        getContent.value = data;
-      });
-  }
-
-  const postContent = ref();
-  function handlePost() {
-    fetch('api', {
-      method: 'post',
-      body: JSON.stringify({ a: 'a', b: 2 })
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        postContent.value = data;
-      });
-  }
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
---
+++ examples/json-test/src/compontent/XiStart.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="xi-"></div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
