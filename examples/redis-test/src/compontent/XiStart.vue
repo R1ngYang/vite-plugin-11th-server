862dfd130048b8b2fedbcdc1c07d47e6ce6d9853
stringyang <stringyang@126.com>
2022-06-17 21:14:22 +0800

    feat: 增加一些代码。

---
+++ examples/redis-test/src/compontent/XiStart.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="xi-start-main"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+  .xi-start-main {
+    width: 100px;
+    height: 100px;
+  }
+</style>
