95c5b2b59f5d1e88c8b0748aab08a8f8f8bca27e
stringyang <stringyang@126.com>
2022-07-11 21:20:31 +0800

    feat: 获取11th-server接口数据。

--- examples/nuxt-text/app.vue
+++ examples/nuxt-text/app.vue
@@ -3,6 +3,16 @@
     helloword
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+  fetch('api', {
+    method: 'post',
+    body: JSON.stringify({ a: 'a', b: 2 })
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+</script>
 
 <style scoped></style>
